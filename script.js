597e99ebbf53652e47ab4b20aaae07f6a8f219c0
ermoiz2102 <115916289+ermoiz2102@users.noreply.github.com>
2024-03-30 21:53:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function fibonacci(num) {
 // your code here
+	if(num<2)
+			return num
+		let a=0
+	     let b=1
+	let ans=0
+	while(num>1){
+		ans=a+b
+		a=b
+		b=ans
+	}
+	return b
+	  
 }
 
 module.exports = fibonacci;
