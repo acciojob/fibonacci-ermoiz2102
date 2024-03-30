e7055befe1a833a5c079cdd8ab994ed583a382d5
ermoiz2102 <115916289+ermoiz2102@users.noreply.github.com>
2024-03-30 21:57:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function fibonacci(num) {
 // your code here
-	if(num<2)
-			return num
+	if(num<=2)
+			return num-1
 		let a=0
 	     let b=1
 	let ans=0
-	while(num>1){
+	while(num-->2){
 		ans=a+b
 		a=b
 		b=ans
